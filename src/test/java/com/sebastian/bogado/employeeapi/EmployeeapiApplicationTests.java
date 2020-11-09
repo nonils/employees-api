6f6b9a997391c6d079573be30591068e1d58f2d9
No Nils <nonils@tutanota.com>
2020-11-08 23:58:03 -0300

    Innitial commit

---
+++ src/test/java/com/sebastian/bogado/employeeapi/EmployeeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.bogado.employeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
